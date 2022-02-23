06ab42378a8bae26eb7268a07d2387151dc639a2
emanoeldelfino <emanoeldelfno@gmail.com>
2022-02-23 15:07:31 -0300

    Add recursive fibonacci term calc

---
+++ C/recursion/fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int fib(int terms);
+
+int main(int argc, char *argv[]) {
+    int num;
+    do {
+        printf("\nEnter a term for Fibonacci (negative for stopping): ");
+        scanf("%d", &num);
+        printf("\nFibonacci(%d) = %d\n", num, fib(num));
+    } while (num >= 0);
+    printf("\n");
+}
+
+int fib(int term) {
+    if (term < 1)
+        return 0;
+    else if (term == 1)
+        return 1;
+    else
+        return fib(term - 1) + fib(term - 2);
+}
+
